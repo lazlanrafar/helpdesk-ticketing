1569300cfe2a187987fcdb66171d62f031c3bdf2
lazlanrafar <lazlanrafar@gmail.com>
2022-08-16 11:58:09 +0700

    create model and migration sub kategori

---
+++ database/migrations/2022_08_16_045654_create_sub_kategoris_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_kategoris', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_kategori');
+            $table->string('nama_kategori');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_kategoris');
+    }
+};
