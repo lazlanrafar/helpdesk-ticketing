008f8482021eb3dd801d421911f62c8de69eb3ae
lazlanrafar <lazlanrafar@gmail.com>
2022-08-27 22:25:54 +0700

    clone

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->integer('id_karyawan');
+            $table->string('email')->unique();
             $table->string('username')->unique();
             $table->string('password');
             $table->string('level');
