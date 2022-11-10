2131dbe484b4d8dd40b9e5cc78cc1b4f17d85763
rahmadh1610 <rahmadofclash@gmail.com>
2022-11-10 09:22:22 +0700

    dashboard

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app') @section('content')
+<meta http-equiv="refresh" content="60" />
 <div class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
@@ -8,6 +9,62 @@
         </div>
     </div>
 </div>
+
+@if(auth()->user()->level == 'STAFF')
+<div class="col-md">
+    <div class="card card-default">
+      <div class="card-header">
+        <h3 class="card-title">
+          <i class="fas fa-bullhorn"></i>
+          <b>CARA MENGGUNAKAN APLIKASI HELPDESK TICKETING</b>
+        </h3>
+      </div>
+      <!-- /.card-header -->
+      <div class="card-body">
+        <div class="callout callout-success">
+          <h5>1. Tekan menu pengaduan</h5>
+
+          <p><img src="{{ url('/1.png') }}" class="img-fluid"></p>
+        </div>
+      </div>
+      <div class="card-body">
+        <div class="callout callout-success">
+          <h5>2. Tekan tombol tambah</h5>
+
+          <p><img src="{{ url('/2.png') }}" class="img-fluid"/></p>
+        </div>
+      </div>
+      <div class="card-body">
+        <div class="callout callout-success">
+          <h5>3. Isi data sesuai dengan keluhan anda, jika data sudah diisi kemudian tekan tombol simpan</h5>
+
+          <p><img src="{{ url('/3.png') }}" class="img-fluid"/></p>
+        </div>
+      </div>
+      <div class="card-body">
+        <div class="callout callout-success">
+          <h5>4. Keluhan akan di record dan dikirimkan kepada teknisi untuk di proses</h5>
+
+          <p><img src="{{ url('/4.png') }}" class="img-fluid"/></p>
+        </div>
+      </div>
+      <div class="card-body">
+        <div class="callout callout-success">
+          <h5>5. Jika keluhan sudah selesai di proses, maka status akan berubah menjadi close</h5>
+
+          <p><img src="{{ url('/5.png') }}" class="img-fluid"/></p>
+        </div>
+      </div>
+      <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+  </div>
+  <!-- /.col -->
+</div>
+<!-- /.row -->
+@endif
+
+@if(auth()->user()->level != 'STAFF')
 <section class="content">
     <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
@@ -59,7 +116,7 @@
             <div class="col-md-12">
                 <div class="card card-info">
                     <div class="card-header">
-                        <h3 class="card-title">Summary SLA</h3>
+                        <h3 class="card-title">Summary Kinerja Tim Helpdesk</h3>
                     </div>
                     <div class="card-body">
                         <div class="chart">
@@ -81,4 +138,5 @@
         </div>
     </div>
 </section>
+@endif
 @endsection
